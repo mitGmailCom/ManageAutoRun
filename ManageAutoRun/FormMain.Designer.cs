e54e7def363b55ffa05db30e45989035b2a633f5
Admin <Admin@X>
2018-10-09 16:10:24 +0300

    Finished application

--- ManageAutoRun/FormMain.Designer.cs
+++ ManageAutoRun/FormMain.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxProgramms = new System.Windows.Forms.ListBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List of programms in AutoRun:";
+            // 
+            // listBoxProgramms
+            // 
+            this.listBoxProgramms.FormattingEnabled = true;
+            this.listBoxProgramms.Location = new System.Drawing.Point(-1, 17);
+            this.listBoxProgramms.Name = "listBoxProgramms";
+            this.listBoxProgramms.ScrollAlwaysVisible = true;
+            this.listBoxProgramms.Size = new System.Drawing.Size(285, 199);
+            this.listBoxProgramms.TabIndex = 1;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(-1, 222);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(139, 32);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(143, 222);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(139, 32);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.listBoxProgramms);
+            this.Controls.Add(this.label1);
             this.Name = "FormMain";
             this.Text = "Manage AutoRun";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxProgramms;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
     }
 }
 
--- ManageAutoRun/FormMain.cs
+++ ManageAutoRun/FormMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,130 @@ namespace ManageAutoRun
 {
     public partial class FormMain : Form
     {
+        private RegistryKey currentUser;
+        private RegistryKey software;
+        private RegistryKey microsoft;
+        private RegistryKey windows;
+        private RegistryKey currentVersion;
+        private RegistryKey run;
+
         public FormMain()
         {
             InitializeComponent();
+            Load += FormMain_Load;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterScreen;
+            LoadPrograms();
+        }
+
+        /// <summary>
+        /// Invoke OpenSubKeys and SetProgrammsToListBox
+        /// </summary>
+        private void LoadPrograms()
+        {
+            OpenSubKeys();
+            SetProgrammsToListBox();
+        }
+
+        /// <summary>
+        /// Open Autorun
+        /// </summary>
+        private void OpenSubKeys()
+        {
+            currentUser = Registry.CurrentUser;
+            software = currentUser.OpenSubKey("SOFTWARE", true);
+            microsoft = software.OpenSubKey("Microsoft", true);
+            windows = microsoft.OpenSubKey("Windows", true);
+            currentVersion = windows.OpenSubKey("CurrentVersion", true);
+            run = currentVersion.OpenSubKey("Run", true);
+        }
+
+        /// <summary>
+        /// Set programms from autorun in listbox
+        /// </summary>
+        private void SetProgrammsToListBox()
+        {
+            var mass = run.GetValueNames();
+            listBoxProgramms.Items.AddRange(mass);
+        }
+
+
+
+        /// <summary>
+        /// Click on "Delete"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (listBoxProgramms.Items.Count != 0)
+            {
+                if (listBoxProgramms.SelectedItem != null)
+                    run.DeleteValue($"{listBoxProgramms.SelectedItem.ToString()}");
+                else
+                    MessageBox.Show("Select one programm to delete from auturun", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            UpdateListBox();
+        }
+
+        /// <summary>
+        /// Click on "Add"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string path = GetPath();
+            string name = path.Substring(path.LastIndexOf('\\') + 1);
+
+            run.SetValue(name, path);
+            UpdateListBox();
+        }
+
+
+
+        /// <summary>
+        /// Get path to selected programm
+        /// </summary>
+        /// <returns></returns>
+        private string GetPath()
+        {
+            string path = string.Empty;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Exe | *.exe";
+            ofd.InitialDirectory = @"C:\Program Files";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// Update values in listbox
+        /// </summary>
+        private void UpdateListBox()
+        {
+            listBoxProgramms.Items.Clear();
+            SetProgrammsToListBox();
+        }
+
+        /// <summary>
+        /// Close all subkeys
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            run.Close();
+            currentVersion.Close();
+            windows.Close();
+            microsoft.Close();
+            software.Close();
+            currentUser.Close();
         }
     }
 }
